eb35233cdafdb648d0c684b2bea4a1d9b9645d2b
WongJingGit <WongJingGit@163.com>
2024-12-28 05:57:20 +0800

    feat(layout): 实现微信登录和多账号切换功能
    - 添加 RadioGroup 和 List 组件用于展示和选择微信账号
    - 实现微信登录按钮和登录逻辑
    -优化聊天窗口样式和功能
    - 添加 UUID 用于消息唯一标识- 重构部分代码以支持新功能

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,9 +1,40 @@
-import React, {useEffect, useMemo, useState} from 'react';
-import {Layout, Row, Col, Space, Input, Typography, Button, Form, Toast, Chat} from '@douyinfe/semi-ui';
+import React, {useEffect, useState} from 'react';
+import {
+    Layout,
+    Row,
+    Col,
+    Space,
+    Typography,
+    Button,
+    Toast,
+    Chat,
+    List,
+    Avatar,
+    RadioGroup, Radio
+} from '@douyinfe/semi-ui';
+import { IconUserCircle, IconUserAdd } from '@douyinfe/semi-icons';
+import { v4 as uuidv4 } from 'uuid';
+import './layout.css'
 
 
 export default function LayoutPage() {
-    const [userConfig, setUserConfig] = useState([]);
+    const [bots, setBots] = useState([]);
+    const [loginLoading, setLoginLoading] = useState(false);
+    const [roleConfig, setRoleConfig] = useState({
+        user: {
+            name: '用户',
+            avatar: <IconUserCircle />,
+            port: null
+        },
+        assistant: {
+            name: '微信小助手',
+            avatar: 'https://wxa.wxs.qq.com/wxad-design/yijie/phone-chat-icon-1.png',
+        },
+        system: {
+            name: '系统',
+            avatar: 'https://wxa.wxs.qq.com/hing/20220531/phone-chat-icon_0.png',
+        }
+    });
     const [ chatMessages, setChatMessages ] = useState([]);
 
     const { Content } = Layout;
@@ -17,28 +48,119 @@ export default function LayoutPage() {
             if (i !== contacts.length - 1) result += "----\n";
 
         }
-        return result+"\n\n请问您要查找的是哪个联系人？";
+        return contacts.length > 1 ? result+"\n\n请问您要查找的是哪个联系人？" : result;
     }
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:16001/api/bot/list', { method: 'GET' })
+            .then(res => res.json())
+            .then(data => {
+                if (data?.code === 200) {
+                    setBots(data?.data);
+                    if (data?.data.length > 0) {
+                        setRoleConfig({...roleConfig, user: { name: data?.data[0]?.name, avatar: data?.data[0]?.info?.headImage, port: data?.data[0]?.port }})
+                    }
+                } else {
+                    Toast.error(data?.message || '请求失败');
+                }
+            })
+            .catch(err => {
+                Toast.error(err.message || '请求失败');
+            })
+    }, []);
 
     return (
         <Layout style={{ height: '100%', width: '100%' }}>
             <Content style={{ height: '100%', width: '100%', padding: 10 }}>
                 <Row style={{ width: '100%', height: 'calc(100% - 20px)' }}>
-                    <Col span={6} style={{ height: '100%' }}></Col>
                     <Col span={6} style={{ height: '100%' }}>
-
+                        <RadioGroup
+                            style={{width: '100%'}}
+                            value={roleConfig.user.port}
+                            onChange={value => {
+                                const selectedBot = bots.filter(item => item.port === value.target.value)[0]
+                                setRoleConfig({...roleConfig, user: { name: selectedBot?.name, avatar: selectedBot?.info?.headImage, port: selectedBot?.port }})
+                            }}
+                        >
+                            <List
+                                style={{width: '100%'}}
+                                loadMore={
+                                    <div style={{width: '100%', display: 'flex', justifyContent: 'center'}}>
+                                        <Button
+                                            icon={<IconUserAdd/>}
+                                            loading={loginLoading}
+                                            onClick={() => {
+                                                setLoginLoading(true);
+                                                fetch('http://127.0.0.1:16001/api/bot/start', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({  }) })
+                                                    .then(res => res.json())
+                                                    .then(data => {
+                                                        if (data?.code === 200) {
+                                                            const interval = setInterval(() => {
+                                                                fetch('http://127.0.0.1:16001/api/bot/login_heartbeat', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ port: data?.data?.port }) })
+                                                                    .then(res => res.json())
+                                                                    .then(loginStatus => {
+                                                                        if (loginStatus?.data?.status) {
+                                                                            clearInterval(interval);
+                                                                            setLoginLoading(false);
+                                                                            setBots([...bots, {...data?.data, ...loginStatus?.data?.info}]);
+                                                                            window.location.reload();
+                                                                        }
+                                                                    })
+                                                            }, 1000)
+                                                        } else {
+                                                            Toast.error(data?.message || '请求失败');
+                                                        }
+                                                    })
+                                                    .catch(err => {
+                                                        Toast.error(err.message || '请求失败');
+                                                        setLoginLoading(false);
+                                                    })
+                                            }}
+                                        >
+                                            登录微信
+                                        </Button>
+                                    </div>
+                                }
+                            >
+                                {
+                                    bots.length > 0 && bots.map(item => {
+                                        return (
+                                            <List.Item
+                                                header={
+                                                    <Space>
+                                                        <Radio value={item?.port} />
+                                                        <Avatar src={item?.info?.headImage} />
+                                                    </Space>
+                                                }
+                                                main={
+                                                    <Space vertical >
+                                                        <Text strong>{ item?.info?.name }</Text>
+                                                        <Text>微信号：{ item?.info?.account }</Text>
+                                                    </Space>
+                                                }
+                                            />
+                                        )
+                                    })
+                                }
+                            </List>
+                        </RadioGroup>
                     </Col>
                     <Col span={12} style={{ height: '100%' }}>
                         <Chat
                             chats={chatMessages}
-                            style={{ width: '100%' }}
-                            showClearContext
-                            onClear={() => {
-                                setChatMessages([]);
+                            uploadProps={{disabled: true, style: { display: 'none' }}}
+                            style={{ width: '100%', maxWidth: '100%' }}
+                            onMessageDelete={value => {
+                                setChatMessages(chatMessages.filter(item => item.id !== value.id));
                             }}
+                            onMessageReset={message => {
+                                console.log(message)
+                            }}
+                            roleConfig={roleConfig}
                             onMessageSend={
                                 message => {
-                                    const newChatMessages = [...chatMessages, { role: 'user', content: message }]
+                                    const newChatMessages = [...chatMessages, { role: 'user', content: message, createAt: (new Date()).getTime(), id: uuidv4() }];
+                                    newChatMessages.push({role: 'assistant', content: '正在查询...', createAt: (new Date()).getTime(), status: 'loading', id: uuidv4()});
                                     setChatMessages([...newChatMessages]);
 
                                     fetch('http://127.0.0.1:16001/api/ai/chat', {
@@ -47,8 +169,8 @@ export default function LayoutPage() {
                                             'Content-Type': 'application/json'
                                         },
                                         body: JSON.stringify({
-                                            port: 19001,
-                                            messages: newChatMessages
+                                            port: roleConfig.user.port,
+                                            messages: newChatMessages.slice(0,-1)
                                         })
                                     })
                                         .then(res => res.json())
@@ -56,23 +178,26 @@ export default function LayoutPage() {
                                             if (data?.code === 200) {
                                                 const reply = data?.data
                                                 if (typeof reply === 'string') {
-                                                    newChatMessages.push({ role: 'assistant', content: reply })
+                                                    newChatMessages[newChatMessages.length - 1] = {...newChatMessages.at(-1), status: 'success', content: reply};
                                                     setChatMessages([...newChatMessages]);
                                                     return;
                                                 }else if (Array.isArray(reply)) {
-                                                    newChatMessages.push({ role: 'assistant', content: generateMultipleContactsReple(reply) })
+                                                    newChatMessages[newChatMessages.length - 1] = {...newChatMessages.at(-1), status: 'success', content: generateMultipleContactsReple(reply)};
                                                     setChatMessages([...newChatMessages]);
                                                     return;
                                                 }
                                                 return
                                             }
-                                            Toast.error(data?.message || '请求失败');
+                                            newChatMessages[newChatMessages.length - 1] = {...newChatMessages.at(-1), status: 'error', content: data?.message || '请求失败'};
+                                            setChatMessages([...newChatMessages]);
                                         })
                                 }
                             }
                         />
                     </Col>
+                    <Col span={6} style={{ height: '100%' }}>
 
+                    </Col>
                 </Row>
             </Content>
         </Layout>
