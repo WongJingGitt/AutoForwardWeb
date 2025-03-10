b62bc5a80538e724603329740917b3c46a823c87
王英杰 <gc_wangyingjie@ieggtc.com>
2025-03-10 18:46:21 +0800

    feat(Layout): 重构布局组件并添加新功能
    - 重构了 Layout 组件的结构和样式- 添加了登录新账号功能和切换微信功能
    - 新增了聊天框组件和消息列表
    - 优化了会话菜单，增加了搜索功能和新增对话按钮
    - 改进了用户界面，显示当前登录的微信信息

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,71 +1,251 @@
-import {Layout, Flex, Row, Col, Avatar} from 'antd'
-import {Conversations} from "@ant-design/x";
-import {useEffect, useState} from "react";
+import {Layout, Flex, Row, Col, Avatar, Typography, Button, Popover, List, Input, Space, theme } from 'antd';
+import {Toast} from '@douyinfe/semi-ui';
+import { CopyOutlined, DeleteOutlined } from '@ant-design/icons';
+import {Bubble, Conversations} from "@ant-design/x";
+import {useEffect, useState, createContext, useContext} from "react";
 import requests from '../utils/requests'
 
-function ConversationMenu({ selectedWechatBot }) {
-    const [conversations, setConversations] = useState([])
+const MainContext = createContext({});
+
+function ConversationMenu() {
+    const [conversations, setConversations] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
+
+    const {conversationId, setConversationId, selectedWechatBot} = useContext(MainContext);
 
     const getConversations = async () => {
         const response = await requests('api/conversations/list', { port: selectedWechatBot.port })
-        setConversations(response.data?.data || [])
+        setConversations(response?.data?.map(item => ({...item, key: item?.conversation_id, label: item?.summary})) || [])
+        return response?.data || [];
     }
 
     useEffect(() => {
         if (selectedWechatBot?.port) {
-            getConversations()
+            getConversations();
+            setConversationId('');
         }
     }, [selectedWechatBot]);
 
     return (
-        <Flex style={{ height: '100%', width: '100%' }}>
+        <Flex style={{ height: '100%', width: '100%' }} vertical>
+            <div style={{paddingLeft: 10, paddingTop: 12, paddingRight: 10}}>
+                <Button block >新增对话</Button>
+            </div>
+            <div style={{paddingLeft: 10, paddingTop: 12, paddingRight: 10}}>
+                <Input placeholder='搜索对话' onChange={event => setSearchValue(event.target.value)} />
+            </div>
             <Conversations
+                activeKey={conversationId}
                 style={{ height: 'auto', width: '100%', margin: 0 }}
+                items={conversations.filter(item => item.summary.includes(searchValue))}
+                onActiveChange={key => {
+                    setConversationId(key);
+                }}
+            />
+        </Flex>
+    )
+}
+
+
+function ChatBox ({  }) {    
+    const {conversationId, selectedWechatBot} = useContext(MainContext);
+    const MessageTools = ({messageItem}) => {
+        const copyMessage = () => {
+            window.navigator.clipboard.writeText(messageItem.content);
+            Toast.success('已复制!')
+        }
+        return (
+            <Space size={token.paddingXXS}>
+                <Button color="default" variant="text" size="small" icon={<CopyOutlined />} onClick={copyMessage}  />
+                <Button color="danger" variant="text" size="small" icon={<DeleteOutlined />} />
+            </Space>
+        )
+    }
+
+    const [messages, setMessages] = useState([]);
+    const [roleConfig, setRoleConfig] = useState({
+        assistant: { placement: 'start', avatar: {icon: <Avatar src='https://wxa.wxs.qq.com/wxad-design/yijie/phone-chat-icon-1.png' />}} , 
+        user: { placement: 'end', avatar: {icon: <Avatar src={selectedWechatBot?.info?.headImage} />}}
+    })
+
+    const {token} = theme.useToken();
+    const getConversationMsg = id => {
+        return new Promise((resolve, reject) => {
+            if (!id) {
+                resolve([]);
+                return;
+            }
+            requests('api/conversations/messages', { conversation_id: id })
+                .then(data => {
+                    if (data?.code === 200) {
+                        setMessages(data?.data);
+                        resolve(data?.data);
+                    } else {
+                        resolve([]);
+                    }
+                })
+        })
+    }
+
+    
+    
+    useEffect(() => {
+        getConversationMsg(conversationId);
+    }, [conversationId])
+
+    useEffect(() => {
+        setRoleConfig({...roleConfig, user: { ...roleConfig.user, avatar: {icon: <Avatar src={selectedWechatBot?.info?.headImage} />} }})
+    }, [selectedWechatBot])
+
+    return (
+        <Flex vertical gap="middle" style={{maxHeight: '100%', overflow: 'auto'}}>
+            <Bubble.List 
+                roles={roleConfig}
+                items={messages.map(item => {
+                    console.log(item)
+                    return {
+                        ...item,
+                        footer: <MessageTools messageItem={item} />
+                    }
+                })}
             />
         </Flex>
     )
 }
 
 export default function LayoutPage() {
-    const [ selectedWechatBot, setSelectedWechatBot ] = useState({})
-    const [ wechatBotList, setWechatBotList ] = useState([])
+    const [ selectedWechatBot, setSelectedWechatBot ] = useState({});
+    const [ wechatBotList, setWechatBotList ] = useState([]);
+    const [ isLogin, setIsLogin ] = useState(false);
+    const [ conversationId, setConversationId ] = useState('');
 
     const { Sider, Content } = Layout;
-    const getBotList = async () => {
-        const response = await requests('api/bot/list', {}, 'GET')
-        setWechatBotList(response.data?.data || [])
+    const { Text } = Typography;
+
+
+    const getBotList = () => {
+        return new Promise((resolve, reject) => {
+            requests('api/bot/list', {}, 'GET')
+                .then(response => {
+                    setWechatBotList(response?.data || [])
+                    resolve(response?.data || [])
+                })
+        })
     }
+
+    const login = async () => {
+        setIsLogin(true);
+        requests('api/bot/start', {}, 'POST', {}, () => setIsLogin(false))
+            .then(data => {
+                if (data?.code === 200) {
+                    const interval = setInterval(() => {
+                        requests("api/bot/login_heartbeat", { port: data?.data?.port })
+                            .then(loginStatus => {
+                                if (loginStatus?.data?.status) {
+                                    clearInterval(interval);
+                                    setIsLogin(false);
+                                    setWechatBotList([...wechatBotList, data?.data])
+                                    setTimeout(() => {
+                                        window.location.reload();
+                                    }, 1000)
+                                }
+                            })
+                    }, 1000)
+                } else {
+                    Toast.error(data?.message || '请求失败');
+                }
+            })
+            .catch(err => {
+                Toast.error(err.message || '请求失败');
+                setIsLogin(false);
+            })
+    }
+
+    const LoginButton = () => <Button loading={isLogin} onClick={login} > 登录新账号 </Button>
 
     useEffect(() => {
         getBotList()
-            .then(() => {
-                if (Object.keys(selectedWechatBot).length === 0 && wechatBotList.length > 0) {
-                    setSelectedWechatBot(wechatBotList[0])
+            .then(response => {
+                if (response.length > 0) {
+                    setSelectedWechatBot(response[0]);
                 }
-            })
+            });
     }, [])
 
 
     return (
         <Layout style={{ height: '100vh', width: '100vw' }}>
-            <Sider
-                style={{ height: '100%', margin: 0 }}
-                theme="light"
-                collapsible={true}
-            >
-                <Flex vertical={true}>
-                    <Row style={{ width: '100%', height: 65 }}>
-                        <Col flex="65px" style={{ padding: 5 }}>
-                            <Avatar size={60} />
-                        </Col>
-                        <Col flex="auto" style={{ padding: 5 }}>
-                            
-                        </Col>
-                    </Row>
-                    <ConversationMenu selectedWechatBot={selectedWechatBot} />
-                </Flex>
-            </Sider>
-            <Content></Content>
+            <MainContext.Provider value={{ selectedWechatBot, setSelectedWechatBot, conversationId, setConversationId }}>
+                <Sider
+                    style={{ height: '100%', margin: 0 }}
+                    theme="light"
+                    width={260}
+                >
+                    <Flex vertical={true}>
+                        <Row style={{ width: '100%', height: 65 }}>
+                            <Col flex="auto" style={{ padding: '10px 0px 0px 0px', width: 'calc(100% - 85px)', height: '100%', display: 'flex', textAlign: 'center', alignItems: 'center', justifyContent: 'center' }}>
+                                {
+                                    selectedWechatBot?.info?.name ? (
+                                        <div style={{ height: '100%', width: '100%' }}>
+                                            <div style={{height: '50%', display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%'}}>
+                                                <Text strong={true} style={{ width: '100%' }} ellipsis={{rows: 1, tooltip: true}}>{selectedWechatBot?.info?.name ?? '未登录'}</Text>
+                                            </div>
+                                            <div style={{height: '50%', display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%'}}>
+                                                <Text type='secondary' style={{ width: '100%' }} ellipsis={{rows: 1, tooltip: true}}>{selectedWechatBot?.info?.account ?? "未登录"}</Text>
+                                            </div>
+                                        </div>
+                                    ) : (
+                                        <LoginButton />
+                                    )
+                                }
+                            </Col>
+                            <Col flex="65px" style={{ padding: '5px 10px 5px 5px' }}>
+                                {
+                                    wechatBotList.length > 0 ? (
+                                        <Popover
+                                            title="切换微信"
+                                            placement="rightTop"
+                                            fresh={true}
+                                            content={
+                                                <List
+                                                    style={{width: 200, maxHeight: 600, overflow: 'auto'}}
+                                                    itemLayout="horizontal"
+                                                    dataSource={wechatBotList}
+                                                    renderItem={(item, index) => {
+                                                        console.log(item)
+                                                        return (
+                                                            <List.Item>
+                                                                <List.Item.Meta
+                                                                    avatar={<Avatar src={item.info?.headImage} />}
+                                                                    title={<a onClick={() => setSelectedWechatBot(item)} >{item.info?.name || ''}</a>}
+                                                                    description={item.info?.account || '获取账号信息失败'}
+                                                                />
+                                                            </List.Item>
+                                                        )
+                                                    }}
+                                                    footer={
+                                                        <div style={{width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                                                            <LoginButton/>
+                                                        </div>
+                                                    }
+                                                />
+                                            }
+                                        >
+                                            <Avatar style={{cursor: 'pointer'}} size={60} src={selectedWechatBot?.info?.headImage} />
+                                        </Popover>
+                                    ) :(
+                                        <Avatar style={{cursor: 'pointer'}} size={60} src={selectedWechatBot?.info?.headImage} />
+                                    )
+                                }
+                            </Col>
+                        </Row>
+                        <ConversationMenu />
+                    </Flex>
+                </Sider>
+                <Content>
+                    <ChatBox />
+                </Content>
+            </MainContext.Provider>
         </Layout>
     )
 }
