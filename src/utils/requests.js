4e3a6f52a6e0f91362a36bd6df1cc07717ad3439
王英杰 <gc_wangyingjie@ieggtc.com>
2025-03-10 18:45:47 +0800

    feat(utils): 优化请求工具函数
    - 重构 requests 函数，增加配置参数和取消请求功能- 添加请求超时设置和自定义 headers 支持
    -优化错误处理，区分取消请求和其他错误
    - 代码结构更加清晰，功能更加强大

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,33 +1,47 @@
 import axios from "axios";
 
-async function requests (api, data, method = 'POST') {
-    api = `http://127.0.0.1:16001/${api}`
-    switch (method) {
-        case "POST":
-            return await axios.post(api, data, {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-                .then(res => res.data)
-                .catch(err => err.response.data);
-        case "GET":
-            return await axios.get(api, {
-                params: data,
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-                .then(res => res.data)
-                .catch(err => err.response.data);
-        default:
-            return await axios.post(api, data, {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-                .then(res => res.data)
-                .catch(err => err.response.data);
+async function requests(api, data = {}, method = 'POST', config = {}, onCancel = () => {}) {
+    const baseURL = 'http://127.0.0.1:16001';
+    const url = `${baseURL}/${api}`;
+
+    const source = axios.CancelToken.source();
+
+    const mergedConfig = {
+        headers: {
+            'Content-Type': 'application/json',
+            ...config.headers 
+        },
+        timeout: config.timeout || 5000,
+        cancelToken: source.token,
+        ...config
+    };
+
+    try {
+        const requestConfig = {
+            method: method.toUpperCase(),
+            url: url,
+            ...mergedConfig
+        };
+
+        if (method.toUpperCase() === 'GET') {
+            requestConfig.params = data;
+        } else {
+            requestConfig.data = data;
+        }
+
+        const response = await axios.request(requestConfig);
+        return response.data;
+    } catch (error) {
+        if (axios.isCancel(error)) {
+            console.log('Request canceled', error.message);
+            onCancel();
+        } else {
+            if (error.response) {
+                throw error.response.data;
+            } else {
+                throw error;
+            }
+        }
     }
 }
 
